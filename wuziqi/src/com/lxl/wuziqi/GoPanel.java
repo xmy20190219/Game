699eb8e754054d0657cd4bb4cacbf2f4de2a69b4
762605241 <762605241@qq.com>
2017-04-04 21:01:35 +0800

    五子棋人机对战初步完成。

--- wuziqi/src/com/lxl/wuziqi/GoPanel.java
+++ wuziqi/src/com/lxl/wuziqi/GoPanel.java
@@ -7,6 +7,8 @@ import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 import javax.imageio.ImageIO;
@@ -15,7 +17,8 @@ import javax.swing.JPanel;
 public class GoPanel extends JPanel {
 	
 	BufferedImage image;
-	ArrayList<Piece> piecesList = new ArrayList<>();
+	ArrayList<Piece> piecesListB = new ArrayList<>();
+	ArrayList<Piece> piecesListW = new ArrayList<>();
 
 	public GoPanel() {
 		try {
@@ -33,10 +36,37 @@ public class GoPanel extends JPanel {
 		// 棋盘
 		g.drawImage(image, 0, 0, getWidth(), getHeight(), null);
 		// 棋子
-		for (Piece piece : piecesList) {
+		for (Piece piece : piecesListW) {
 			piece.draw(g);
 		}
-		
+		for (Piece piece : piecesListB) {
+			piece.draw(g);
+		}
+		//TODO:落黑子
+		//TODO：判断输赢有问题
+		ArrayList<ArrayList<Piece>> allList = null;
+		allList = PieceHandle.getAllMax(piecesListB, piecesListW);
+		Collections.sort(allList, new Comparator<ArrayList<Piece>>() {
+
+			@Override
+			public int compare(ArrayList<Piece> o1, ArrayList<Piece> o2) {
+				if (o1.size() > o2.size()) {
+					return -1;
+				} else if (o1.size() < o2.size()) {
+					return 1;
+				}
+				return 0;
+			}
+		});
+		for (ArrayList<Piece> list : allList) {
+			if (list.size() == 5) {
+				if (list.get(0).isWhite()) {
+					System.out.println("白棋获胜");
+				} else {
+					System.out.println("黑棋获胜");
+				}
+			}
+		}
 	}
 
 	/**
@@ -59,22 +89,53 @@ public class GoPanel extends JPanel {
 			int px = (x - (x - 15) / 26 * 26 - 15) > 13 ? ((x - 15) / 26 + 1) * 26 + 15 - 10: ((x - 15) / 26 * 26 + 15) - 10;
 			int py = (y - (y - 15) / 26 * 26 - 15) > 13 ? ((y - 15) / 26 + 1) * 26 + 15 - 10: ((y - 15) / 26 * 26 + 15) - 10;
 			Piece piece = new Piece(px, py);
-			if (!piecesList.contains(piece)) {
+			ArrayList<ArrayList<Piece>> allList = null;
+			if (!piecesListW.contains(piece)) {
 				//人落子
 				piece.setWhite(isWhite);
-				piecesList.add(piece);
+				piecesListW.add(piece);
 //				isWhite = !isWhite;
 				/**
 				 * TODO:机器落子
 				 * 1、找出人落子的最长路径
 				 * 2、堵住这个路径
 				 */
-				System.out.print("棋盘上所有棋子\t");
-				for (int i = 0; i < piecesList.size(); i++) {
-					System.out.print("<" + piecesList.get(i).getX() + "," + piecesList.get(i).getY() + ">\t");
+//				System.out.print("棋盘上所有棋子\t");
+//				for (int i = 0; i < piecesListW.size(); i++) {
+//					System.out.print("<" + piecesListW.get(i).getX() + "," + piecesListW.get(i).getY() + ">\t");
+//				}
+//				System.out.println();
+			}
+			//TODO:落黑子
+			//TODO：判断输赢有问题
+			allList = PieceHandle.getAllMax(piecesListB, piecesListW);
+			Collections.sort(allList, new Comparator<ArrayList<Piece>>() {
+
+				@Override
+				public int compare(ArrayList<Piece> o1, ArrayList<Piece> o2) {
+					if (o1.size() > o2.size()) {
+						return -1;
+					} else if (o1.size() < o2.size()) {
+						return 1;
+					}
+					return 0;
 				}
-				System.out.println();
-				PieceHandle.getNextPoint(piecesList);
+			});
+			for (ArrayList<Piece> list : allList) {
+				if (list.size() == 5) {
+					if (list.get(0).isWhite()) {
+						System.out.println("白棋获胜");
+					} else {
+						System.out.println("黑棋获胜");
+					}
+				}else{
+					Piece p = PieceHandle.getNextBlackPiece(list);
+					if (!piecesListB.contains(p) && !piecesListW.contains(p)) {
+						p.setWhite(false);
+						piecesListB.add(p);
+						break;
+					}
+				}
 			}
 			// 重绘
 			repaint();
--- wuziqi/src/com/lxl/wuziqi/PieceHandle.java
+++ wuziqi/src/com/lxl/wuziqi/PieceHandle.java
@@ -1,12 +1,10 @@
 package com.lxl.wuziqi;
 
-import java.awt.Point;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.HashMap;
 import java.util.Iterator;
-import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Set;
@@ -15,7 +13,17 @@ public class PieceHandle {
 	static Map<Integer, ArrayList<Piece>> map;
 	static ArrayList<Piece> maxSizeList = new ArrayList<>();
 	static ArrayList<Piece> pieceList;
-
+	static ArrayList<ArrayList<Piece>> allList = new ArrayList<>();
+	private static ArrayList<Piece> array1;
+	private static ArrayList<Piece> array2;
+	
+	public static ArrayList<ArrayList<Piece>> getAllMax(ArrayList<Piece> a, ArrayList<Piece> b){
+		array1 = a;
+		array2 = b;
+		getMaxList(a);
+		getMaxList(b);
+		return allList;
+	}
 	/**
 	 * 确定电脑先一步落子位置
 	 * 
@@ -23,36 +31,144 @@ public class PieceHandle {
 	 *            棋盘上现有所有棋子
 	 * @return 落子坐标
 	 */
-	public static Point getNextPoint(ArrayList<Piece> list) {
+	public static ArrayList<Piece> getMaxList(ArrayList<Piece> list) {
+
 		pieceList = list;
+		// 判断水平最大值最大值
 		ArrayList<Piece> a = getHorizonArray();
-//		System.out.print("水平最长\t");
-//		for (int i = 0; i < a.size(); i++) {
-//			System.out.print("<" + a.get(i).getX() + " , " + a.get(i).getY() + ">" + "\t");
-//		}
-//		System.out.println();
+		// System.out.print("水平最长\t");
+		// for (int i = 0; i < a.size(); i++) {
+		// System.out.print("<" + a.get(i).getX() + " , " + a.get(i).getY() +
+		// ">" + "\t");
+		// }
+		// System.out.println();
+		// 判断竖直最大值最大值
 		a = getVerticalArray();
-//		System.out.print("竖直最长\t");
-//		for (int i = 0; i < a.size(); i++) {
-//			System.out.print("<" + a.get(i).getX() + " , " + a.get(i).getY() + ">" + "\t");
-//		}
-//		System.out.println();
-		return null;
+		// System.out.print("竖直最长\t");
+		// for (int i = 0; i < a.size(); i++) {
+		// System.out.print("<" + a.get(i).getX() + " , " + a.get(i).getY() +
+		// ">" + "\t");
+		// }
+		// System.out.println();
+		// 判断45°方向最大值
+		a = getDiagonalArray();
+		// for (int i = 0; i < a.size(); i++) {
+		// System.out.print("<" + a.get(i).getX() + " , " + a.get(i).getY() +
+		// ">" + "\t");
+		// }
+		// System.out.println();
+		// for (int i = 0; i < maxSizeList.size(); i++) {
+		// System.out.print("<" + maxSizeList.get(i).getX() + " , " +
+		// maxSizeList.get(i).getY() + ">" + "\t");
+		// }
+		// System.out.println();
+		return maxSizeList;
 	}
 
-	public static void whoWin() {
-		getHorizonArray();
+	private static ArrayList<Piece> getD2MaxSubList(ArrayList<Piece> list) {
+		ArrayList<Piece> temp = list;
+		int x = 0;
+		int y = 0;
+		// 是否添加第一个元素，代表是否为第一次
+		boolean flag = true;
+		ArrayList<Piece> a = new ArrayList<>();
+		for (int i = 0; i < temp.size(); i++) {
+			if (flag) {
+				a.add(temp.get(i));
+			}
+			x = temp.get(i).getX() - 26;
+			y = temp.get(i).getY() - 26;
+			Piece p = new Piece(x, y);
+			while (temp.contains(p)) {
+				a.add(p);
+				x = p.getX() - 26;
+				y = p.getY() - 26;
+				p = new Piece(x, y);
+			}
+			if (a.size() > maxSizeList.size()) {
+				maxSizeList = a;
+			}
+			allList.add(a);
+			a = new ArrayList<>();
+		}
+		return maxSizeList;
 	}
 
-	
 	/**
-	 * 1、得到期盼中所有水平方向上的棋子Map 
-	 * 2、在Map中找到最长的竖直集合
+	 * 遍历所有点 判断是否存在下一个点（该点为右下方最近的一个点）
+	 * 
+	 * @return 返回45°方向最大连续子集
+	 */
+	private static ArrayList<Piece> getDiagonalArray() {
+		ArrayList<Piece> list = pieceList;
+		/**
+		 * 将棋盘上的棋子进行排序 排序方式：从上到下，从左到右
+		 */
+		Collections.sort(list, new Comparator<Piece>() {
+			// 需要改进
+			@Override
+			public int compare(Piece o1, Piece o2) {
+				if (o1.getX() > o2.getX() && o1.getY() > o2.getY()) {
+					return 1;
+				}
+				if (o1.getX() < o2.getX() && o1.getY() < o2.getY()) {
+					return -1;
+				}
+				if (o1.getX() == o2.getX() && o1.getY() > o2.getY()) {
+					return 1;
+				}
+				if (o1.getX() == o2.getX() && o1.getY() < o2.getY()) {
+					return -1;
+				}
+				if (o1.getY() == o2.getY() && o1.getX() > o2.getX()) {
+					return 1;
+				}
+				if (o1.getY() == o2.getY() && o1.getX() < o2.getX()) {
+					return -1;
+				}
+				// 可以没有相等判断（因为不允许棋子落到同一个点）
+				return 0;
+			}
+		});
+		// 判读是否存在下一个点
+
+		// 得到行的最长子集合
+		ArrayList<Piece> a = getD1MaxSubList(list);
+		if (a.size() > maxSizeList.size()) {
+			maxSizeList = a;
+		}
+		a = getD2MaxSubList(list);
+		if (a.size() > maxSizeList.size()) {
+			maxSizeList = a;
+		}
+		return maxSizeList;
+	}
+
+	/**
+	 * 判断输赢
+	 * 
+	 * @param w
+	 * @param b
+	 * @return 返回棋子最多的集合
+	 */
+	public static ArrayList<Piece> whoWin(ArrayList<Piece> w, ArrayList<Piece> b) {
+		if (w.size() == 5) {
+			System.out.println("白棋获胜");
+			return w;
+		}
+		if (b.size() == 5) {
+			System.out.println("黑棋获胜");
+			return b;
+		}
+		return b.size() > w.size() ? b : w;
+	}
+
+	/**
+	 * 1、得到期盼中所有水平方向上的棋子Map 2、在Map中找到最长的竖直集合
 	 * 
 	 * @return
 	 */
-	private static ArrayList<Piece> getVerticalArray() {
-		System.out.println("------------------------------------------------------------------------");
+	private static ArrayList<Piece> getHorizonArray() {
 		/**
 		 * 1、将棋子列表中的棋子以棋子的X坐标分组
 		 */
@@ -76,13 +192,13 @@ public class PieceHandle {
 			// 有几行执行几次
 			Entry<Integer, ArrayList<Piece>> entry = it.next();
 			ArrayList<Piece> tempList = entry.getValue();
-			
-//			for (int i = 0; i < tempList.size(); i++) {
-//				System.out.println(tempList.get(i));
-//			}
-//			System.out.println("");
-			
-			//将同一行的棋子以Y坐标排序
+
+			// for (int i = 0; i < tempList.size(); i++) {
+			// System.out.println(tempList.get(i));
+			// }
+			// System.out.println("");
+
+			// 将同一行的棋子以Y坐标排序
 			Collections.sort(tempList, new Comparator<Piece>() {
 
 				@Override
@@ -92,38 +208,36 @@ public class PieceHandle {
 					} else if (o1.getY() < o2.getY()) {
 						return -1;
 					} else {
+						// 可以没有相等判断（因为不允许棋子落到同一个点）
 						return 0;
 					}
 				}
 			});
-			System.out.println("竖直方向总集合为");
-			for (int i = 0; i < tempList.size(); i++) {
-				System.out.print(tempList.get(i).getY() + "\t");
-			}
-			System.out.println();
+			// System.out.println("竖直方向总集合为");
+			// for (int i = 0; i < tempList.size(); i++) {
+			// System.out.print(tempList.get(i).getY() + "\t");
+			// }
+			// System.out.println();
 			// 得到行的最长子集合
 			ArrayList<Piece> a = getVMaxSubList(tempList);
-			System.out.println("竖直方向最长的子集合");
-			for (int i = 0; i < a.size(); i++) {
-				System.out.print(a.get(i).getY() + "\t");
-			}
-			System.out.println();
+			// System.out.println("竖直方向最长的子集合");
+			// for (int i = 0; i < a.size(); i++) {
+			// System.out.print(a.get(i).getY() + "\t");
+			// }
+			// System.out.println();
 			if (a.size() > maxSizeList.size()) {
 				maxSizeList = a;
 			}
 		}
 		return maxSizeList;
 	}
-	
-	
+
 	/**
-	 * 1、得到期盼中所有水平方向上的棋子Map 
-	 * 2、在Map中找到最长的水平集合
+	 * 1、得到期盼中所有竖直方向上的棋子Map 2、在Map中找到最长的水平集合
 	 * 
 	 * @return
 	 */
-	private static ArrayList<Piece> getHorizonArray() {
-		System.out.println("------------------------------------------------------------------------");
+	private static ArrayList<Piece> getVerticalArray() {
 		/**
 		 * 1、将棋子列表中的棋子以棋子的Y坐标分组
 		 */
@@ -147,7 +261,7 @@ public class PieceHandle {
 			// 有几行执行几次
 			Entry<Integer, ArrayList<Piece>> entry = it.next();
 			ArrayList<Piece> tempList = entry.getValue();
-			//将同一行的棋子以X坐标排序
+			// 将同一行的棋子以X坐标排序
 			Collections.sort(tempList, new Comparator<Piece>() {
 
 				@Override
@@ -157,22 +271,23 @@ public class PieceHandle {
 					} else if (o1.getX() < o2.getX()) {
 						return -1;
 					} else {
+						// 可以没有相等判断（因为不允许棋子落到同一个点）
 						return 0;
 					}
 				}
 			});
-			System.out.println("水平方向总集合为");
-			for (int i = 0; i < tempList.size(); i++) {
-				System.out.print(tempList.get(i).getX() + "\t");
-			}
-			System.out.println();
+			// System.out.println("水平方向总集合为");
+			// for (int i = 0; i < tempList.size(); i++) {
+			// System.out.print(tempList.get(i).getX() + "\t");
+			// }
+			// System.out.println();
 			// 得到行的最长子集合
 			ArrayList<Piece> a = getHMaxSubList(tempList);
-			System.out.println("水平方向最长的子集合");
-			for (int i = 0; i < a.size(); i++) {
-				System.out.print(a.get(i).getX() + "\t");
-			}
-			System.out.println();
+			// System.out.println("水平方向最长的子集合");
+			// for (int i = 0; i < a.size(); i++) {
+			// System.out.print(a.get(i).getX() + "\t");
+			// }
+			// System.out.println();
 			if (a.size() > maxSizeList.size()) {
 				maxSizeList = a;
 			}
@@ -217,16 +332,19 @@ public class PieceHandle {
 			}
 		}
 		if (isAllOrder) {
+			allList.add(temp);
 			return temp;
 		} else if (isNoOrder) {
 			ArrayList<Piece> result = new ArrayList<>();
 			result.add(temp.get(0));
+			allList.add(result);
 			return result;
 		} else {
+			allList.add(max);
 			return max;
 		}
 	}
-	
+
 	/**
 	 * 得到水平数组中 最大连续 的集合
 	 * 
@@ -264,13 +382,98 @@ public class PieceHandle {
 			}
 		}
 		if (isAllOrder) {
+			allList.add(temp);
 			return temp;
 		} else if (isNoOrder) {
 			ArrayList<Piece> result = new ArrayList<>();
 			result.add(temp.get(0));
+			allList.add(result);
 			return result;
 		} else {
+			allList.add(max);
 			return max;
+		}
+	}
+
+	/**
+	 * 得到45°数组中 最大连续 的集合
+	 * 
+	 * @param temp
+	 * @return 最大连续子集合
+	 */
+	private static ArrayList<Piece> getD1MaxSubList(ArrayList<Piece> list) {
+		ArrayList<Piece> temp = list;
+		int x = 0;
+		int y = 0;
+		// 是否添加第一个元素，代表是否为第一次
+		boolean flag = true;
+		ArrayList<Piece> a = new ArrayList<>();
+		for (int i = 0; i < temp.size(); i++) {
+			if (flag) {
+				a.add(temp.get(i));
+			}
+			x = temp.get(i).getX() + 26;
+			y = temp.get(i).getY() + 26;
+			Piece p = new Piece(x, y);
+			while (temp.contains(p)) {
+				a.add(p);
+				x = p.getX() + 26;
+				y = p.getY() + 26;
+				p = new Piece(x, y);
+			}
+			if (a.size() > maxSizeList.size()) {
+				maxSizeList = a;
+			}
+			allList.add(a);
+			a = new ArrayList<>();
+		}
+		return maxSizeList;
+	}
+
+	public static Piece getNextBlackPiece(ArrayList<Piece> list) {
+		if (list.size() == 1) {
+			// 默认水平、右侧添加下一个黑棋
+			// 如果x到达边界则向左侧添加
+			int x = list.get(0).getX();
+			if (x < 473) {
+				x += 26;
+			} else {
+				x -= 26;
+			}
+			return new Piece(x, list.get(0).getY());
+		} else {
+			// 判断方向（水平、竖直、45°）
+			int x = list.get(list.size() - 1).getX() - list.get(0).getX();
+			int y = list.get(list.size() - 1).getY() - list.get(0).getY();
+			if (x == 0) {// 竖直
+				y = list.get(list.size() - 1).getY();
+				if (y < 473 && y > 5) {
+					y += 26;
+					Piece p = new Piece(list.get(list.size() - 1).getX(), y);
+					if (array1.contains(p) || array2.contains(p)) {
+						y = list.get(0).getY();
+					}
+					return p;
+				} else {
+					return null;
+				}
+			} else if (y == 0) {// 水平
+				x = list.get(list.size() - 1).getX();
+				if (x < 473) {
+					x += 26;
+					return new Piece(x, list.get(0).getY());
+				} else {
+					return new Piece(list.get(0).getX() - 26, list.get(0).getY());
+				}
+			} else {// 45°
+				x = list.get(list.size() - 1).getX();
+				y = list.get(list.size() - 1).getY();
+				if (x < 473 && y < 473) {
+					return new Piece(x + 26, y + 26);
+				} else {
+					return new Piece(list.get(0).getX() - 26, list.get(0).getY() - 26);
+				}
+			}
 		}
 	}
 }
